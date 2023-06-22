9cb883a0a85aed96a91650b0cceb45e6dd88e048
lugia19 <lugia19@lugia19.com>
2023-06-22 23:59:11 +0200

    ...Avoid creating three copies of the whisper model.

--- interpreterComponents/detector.py
+++ interpreterComponents/detector.py
@@ -1,3 +1,6 @@
+import datetime
+import gc
+import os
 import platform
 import queue
 import threading
@@ -17,18 +20,8 @@ class Detector:
         self.microphoneInfo = helper.get_portaudio_device_info_from_name(inputDeviceName, "input")
         self.srMic = sr.Microphone(device_index=self.microphoneInfo["index"], sample_rate=int(self.microphoneInfo["default_samplerate"]))
         self.srRecognizer = sr.Recognizer()
-        self.languageOverride = ""
-        self.runLocal = runLocal
-
-        if self.runLocal:
-            if platform.system() == "Linux" or platform.system() == "Windows":
-                self.model = faster_whisper.WhisperModel(modelSize, device="auto", compute_type="float16")
-            else:
-                self.model = faster_whisper.WhisperModel(modelSize, device="auto")
-        else:
-            openai.api_key = apiKey
-
-
+        self.srRecognizer.non_speaking_duration = 0.5
+        print(srSettings)
         self.srRecognizer.energy_threshold = int(srSettings[0])
         self.srRecognizer.dynamic_energy_threshold = srSettings[1]
         self.srRecognizer.pause_threshold = float(srSettings[2])
@@ -36,11 +29,20 @@ class Detector:
         self.interruptEvent = threading.Event()
         self.isRunning = threading.Event()  #This one stops audio detection entirely when cleared.
         self.isRunning.set()
+        self.recognizerData = None
 
         global wRecognizer
-        if wRecognizer is None:
-            wRecognizer = Recognizer(runLocal, self.interruptEvent, modelSize, apiKey)
-            threading.Thread(target=wRecognizer.main_loop).start()
+        with GIL:
+            if wRecognizer is None and (apiKey is not None or modelSize is not None):
+                self.recognizerData = dict()
+                helper.print_usage_info("Before recognizer init")
+                wRecognizer = Recognizer(runLocal, modelSize, apiKey)
+                self.recognizerData["thread"] = threading.Thread(target=wRecognizer.main_loop)
+                self.recognizerData["event"] = wRecognizer.interruptEvent
+                helper.print_usage_info("After recognizer init")
+            elif wRecognizer is None and apiKey is None and modelSize is None:
+                #UGLY UGLY UGLY Fake wRecognizer for settings testing.
+                wRecognizer = type("", (), {"audioQueue":tlQueue})()
 
         self.resultQueue = tlQueue
         self.cloneQueue:queue.Queue = cloneQueue
@@ -51,23 +53,36 @@ class Detector:
         with self.srMic as source:
             self.srRecognizer.adjust_for_ambient_noise(source)
             GIL.release()
+            global wRecognizer
             while True:
                 self.isRunning.wait()  #Wait until we're running. This ensures that we don't accidentally record while muted.
                 print("Detecting audio...")
-                if self.interruptEvent.is_set():
-                    print("Detector exiting...")
-                    break
                 try:
-                    audio:sr.AudioData = self.srRecognizer.listen(source, timeout=30)
+                    audio:sr.AudioData = self.srRecognizer.listen(source, timeout=20, phrase_time_limit=60)
+                    #If you manage to speak a single sentence longer than 1 minute, congrats and f*** you.
+                    #This is to force it to exit in cases with high background noise, which gets detected as speech.
+                    #I have no idea what a better fix would be.
                 except sr.WaitTimeoutError:
                     print("Audio rec interrupted")
+                    if self.interruptEvent.is_set():
+                        print("Detector exiting...")
+                        del wRecognizer
+                        gc.collect()
+                        wRecognizer = None
+                        break
                     continue
 
                 print(f"Audio detected on {self.srMic.device_index}.")
-                audioData = {"audio":audio,"queue":self.resultQueue}
+                audioData = {
+                    "audio":audio.get_wav_data(),
+                    "queue":self.resultQueue,
+                    "endTime": datetime.datetime.now()
+                }
 
                 if self.cloneQueue is not None:
                     audioData["clonequeue"] = self.cloneQueue
-
-                wRecognizer.audioQueue.put_nowait(audioData)
+                if wRecognizer is not None:
+                    wRecognizer.audioQueue.put_nowait(audioData)
+                else:
+                    print("wRecognizer is none.")
 
